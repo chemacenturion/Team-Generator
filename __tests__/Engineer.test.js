6608e6056c037ffa5a72b2161f8ce2d2fcebfb87
chemacenturion <centurion.philadelphia@gmail.com>
2021-08-11 20:52:46 -0400

    pushing successful Engineer tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+const { TestWatcher } = require("jest");
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+
+test("Can set GitHub address with constructor function", () => {
+    const testValue = "https://github.com/chemacenturion";
+    const e = new Engineer("Jose", 4, "centurion.philadelphia@gmail.com", "https://github.com/chemacenturion");
+    expect(e.getGitHub()).toBe(testValue);
+});
+
+test("getRole() with a return of Engineer", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Jose", 4, "centurion.philadelphia@gmail.com", "https://github.com/chemacenturion");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub address with getGitHub()", () => {
+    const testValue = "https://github.com/chemacenturion";
+    const e = new Engineer("Jose", 4, "centurion.philadelphia@gmail.com", testValue);
+    expect(e.getGitHub()).toBe(testValue);
+})
+
